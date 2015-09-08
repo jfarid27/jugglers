f3f21ce2d7859773292eb94ddc30240cc355eb3c
J. Farid <jrab227@gmail.com>
2015-09-07 22:48:11 -0400

    Added line parser

---
+++ src/parseFileLine.js
@@ -0,0 +1,51 @@
+if (typeof define !== 'function') {
+    var define = require("amdefine")(module)
+}
+define(function (require, exports, module) {
+    module.exports = function() {
+        var parseFileLine = function() {
+            return {}
+        }
+
+        parseFileLine.circuit = function(line) {
+            var circuitObj = {
+                code: undefined,
+                score: {
+                    "H": undefined,
+                    "E": undefined,
+                    "P": undefined
+                }
+            }
+            var splits = line.split(" ")
+            circuitObj.code = splits[1]
+            circuitObj.score.H = parseInt(splits[2].split(":")[1])
+            circuitObj.score.E = parseInt(splits[3].split(":")[1])
+            circuitObj.score.P = parseInt(splits[4].split(":")[1])
+            return circuitObj
+        }
+
+        parseFileLine.juggler = function(line) {
+            var jugglerObj = {
+                code: undefined,
+                score: {
+                    "H": undefined,
+                    "E": undefined,
+                    "P": undefined
+                },
+                preferences: []
+            }
+            var splits = line.split(" ")
+            jugglerObj.code = splits[1]
+            jugglerObj.score.H = parseInt(splits[2].split(":")[1])
+            jugglerObj.score.E = parseInt(splits[3].split(":")[1])
+            jugglerObj.score.P = parseInt(splits[4].split(":")[1])
+            splits[5].split(",").map(function(circuit) {
+                jugglerObj.preferences.push(circuit)
+            })
+            return jugglerObj
+        }
+
+        return parseFileLine;
+
+    }
+})
--- test/parseFileLineSpec.js
+++ test/parseFileLineSpec.js
@@ -4,41 +4,38 @@ if (typeof define !== 'function') {
 define(function (require, exports, module) {
     describe("parseFileLine", function() {
         var parseFileLine = require("src/parseFileLine")();
-        describe("scenario when given a circuit line", function() {
-            var test
-            beforeEach(function() {
-                test = "C C0 H:7 E:7 P:10"
-            })
-            it("should cb a proper circuit object", function(done) {
-                var cb = function(result){
-                    expect(result.code).toBe("C0")
-                    expect(result.score.H).toBe("7")
-                    expect(result.score.E).toBe("7")
-                    expect(result.score.P).toBe("10")
-                    done()
-                }
+        describe("circuit method", function() {
+            describe("scenario when given a circuit line", function() {
+                var test, result
+                beforeEach(function() {
+                    test = "C C0 H:7 E:7 P:10"
+                    result = parseFileLine.circuit(test)
+                })
+                it("should return a proper circuit object", function() {
 
-                parseFileLine.circuit(test, cb)
+                    expect(result.code).toBe("C0")
+                    expect(result.score.H).toBe(7)
+                    expect(result.score.E).toBe(7)
+                    expect(result.score.P).toBe(10)
+                })
             })
         })
-        describe("scenario when given a juggler line", function() {
-            var test
-            beforeEach(function() {
-                test = "J J1 H:4 E:3 P:7 C0,C2,C1"
-            })
-            it("should cb a proper juggler object", function(done) {
-                var cb = function(result){
+        describe("juggler method", function() {
+            describe("scenario when given a juggler line", function() {
+                var test, result
+                beforeEach(function() {
+                    test = "J J1 H:4 E:3 P:7 C0,C2,C1"
+                    result = parseFileLine.juggler(test)
+                })
+                it("should return a proper juggler object", function() {
                     expect(result.code).toBe("J1")
-                    expect(result.score.H).toBe("4")
-                    expect(result.score.E).toBe("3")
-                    expect(result.score.P).toBe("7")
+                    expect(result.score.H).toBe(4)
+                    expect(result.score.E).toBe(3)
+                    expect(result.score.P).toBe(7)
                     expect(result.preferences).toContain("C0")
                     expect(result.preferences).toContain("C1")
                     expect(result.preferences).toContain("C2")
-                    done()
-                }
-
-                parseFileLine.juggler(test, cb)
+                })
             })
         })
     })
