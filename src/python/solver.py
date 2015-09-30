c025a503528f590b57188d6ce41436f29e5f9973
J. Farid <jrab227@gmail.com>
2015-09-29 23:03:37 -0400

    added more sim annealing code

--- src/python/solver.py
+++ src/python/solver.py
@@ -1,22 +1,48 @@
-import json
-from scipy import sparse
-import numpy
-import cvxpy as cvx
+import json, random
+import numpy as np
+
+def mult(x, y):
+    return reduce(lambda agg, x: agg + x,[x * y for (x, y) in zip(x, y)], 0)
 
 def vectorPotential(c):
-    g = lambda x: c * x
+    g = lambda x: mult(c, x)
     return g
+
+def jugglersInCircuit(circuitIndex, jugglers, x):
+    jugglers = [((circuitIndex * jugglers) + i, i) for (i, x) in enumerate(x[circuitIndex * jugglers:(circuitIndex+1) * jugglers]) if (x == 1)]
+    return jugglers
+
+def newStateMaker(circuits, jugglers):
+    def g(x):
+        randCircuit1Index = random.randint(0, circuits - 1)
+        randCircuit2Index = random.randint(0, circuits - 1)
+        if (randCircuit1Index == randCircuit2Index):
+            return x
+        randJugglerIndex1 = random.randint(0, float(jugglers)/float(circuits) - 1)
+        randJugglerIndex2 = random.randint(0, float(jugglers)/float(circuits) - 1)
+        jugglersInCircuit1 = jugglersInCircuit(randCircuit1Index, jugglers, x)
+        jugglersInCircuit2 = jugglersInCircuit(randCircuit2Index, jugglers, x)
+        (leavingIndex1, juggler1) = jugglersInCircuit1[randJugglerIndex1]
+        (leavingIndex2, juggler2) = jugglersInCircuit2[randJugglerIndex2]
+        x[leavingIndex1] = 0
+        x[leavingIndex2] = 0
+        x[(randCircuit1Index * jugglers) + juggler2] = 1
+        x[(randCircuit2Index * jugglers) + juggler1] = 1
+        return x
+    return g
+
+
 
 class SimulatedAnnealing():
     def simulate(nsteps, T, V, changeStep, state, generateNewState, gamma):
-        for step in nsteps:
-            if (step == changeStep):
-                T = T - gamma
+        for step in range(nsteps):
+            if step == changeStep:
+                T *= ( 1 - gamma )
             newState = generateNewState(state)
             transitionOdds = math.exp( - ( V(newState) - V(state)  ) / T)
-            if random.uniform(0, 1) < transitionOdds:
+            if random.uniform(0.0, 1.0) < transitionOdds:
                 state = newState
-        return state
+        return T, state
 
 if (__name__ == "__main__"):
     bFile = open("./data/B.json")
@@ -24,23 +50,26 @@ if (__name__ == "__main__"):
     cFile = open("./data/C.json")
     cMat = json.load(cFile)
 
-    nRowsA = 2 * bMat[2] + bMat[0]
     nColsA = bMat[2] * bMat[0]
 
-    C = sparse.lil_matrix((1, nColsA))
+    C = np.zeros((1, nColsA))
 
     #Load up C
     for x in cMat:
         C[0, x[0]] = x[1]
 
-    x = sparse.lil_matrix((1, nColsA))
+    x = np.zeros((1, nColsA))
+
+    #Load up x
+
+    randomState = newStateMaker(bMat[0], bMat[1])
 
     simulationInstance = SimulatedAnnealing()
-    minimized = simulationInstance.simulate(10^9, 10^8, vectorPotential, 1000, x, randomState, 100)
+    minimized = simulationInstance.simulate(10^9, 10^8, vectorPotential(C), 1000, x, randomState, 100)
 
     resultFile = open('./data/X.json', 'w')
     json.dump(minimized.tolist(), resultFile)
-    
+
     resultFile.close
     bFile.close
     cFile.close
